aef9319d3112e9a713f87fc2590b5b0c1a63de40
Kim <akk0425@naver.com>
2022-12-26 17:52:14 +0900

    There is the first file

---
+++ 01.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<bitset>
+
+
+using namespace std;
+
+int main() {
+	// 목적 : 10이라는 숫자를 8번을 곱해서 출력하기 (반복문 없이)
+	
+	//int number = 10;
+	//const int b = 10;
+
+
+	// 힌트 : 1을 100번 더한다.
+	// 목적 : 1부터 100까지 더한 결과를 출력하기
+	
+	// 변수 외 풀어서 생각해보자. - 하.. 이건 다시해보자
+
+	/* int a = 0;  // a+a+1+a+1+1+.....* 100번
+	int b = 0;
+	for (; b < 100; b++) {
+		 //while (b++<100){
+			 a += b;
+
+		//cout << a << endl; 
+		
+	}
+
+		std:: cout << a << endl; */
+
+
+
+// 목적 : 트리만들기 5줄
+
+
+
+/*
+	int count = 0;
+	do {
+		cout << "hello World!" << endl;
+	} while (count++ < 10);
+
+
+
+	for (int count = 0; count < 10; count++) {
+		cout << "hello world!" << endl;
+	}
+
+
+	int count = 0;
+	while (count <10) {
+		cout << count << endl;
+
+	}
+
+	cout << count << endl;
+	*/
+
+	return 0;
+ }
